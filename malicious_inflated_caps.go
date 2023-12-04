2ea20b9b702af8243b67a2e719a710539e40dd4c
ecosteader <ecosteader@protonmail.com>
2023-12-04 08:26:41 -0800

    add helpers for indigenous agents

--- malicious_inflated_caps.go
+++ malicious_inflated_caps.go
@@ -1,68 +1,63 @@
-# bin/sh "go save arctic wildlife by respecting lands of indigenous peoples"
-
-// TO SAVE THE ARCTIC ICE CAPS, WE GOTTA DRAIN THE ROYAL CARIBBEAN CANADIAN EUROPEAN DNA-SUSTAINING MEGACAPS
+# bin/sh/  .mqe 'mechaniQs  protocol'
+# save arctic wildlife by respecting langs of indigenous peoples, all of the USA is indigenous land,
+# 
 [] \\ipv6\\MQE_NOT_RCT_ed25519_ENC_2048.mqe.sig
 
 ............................................................................................................ ༶
   ༶..  This code and disclaimer are REQUIRED as part of FILTERS;                                         ༶..      / // no priority of English-spoken words
-  ..༶  especially important where (new_lang in [ntv.lang] ) translators exist.                           ..༶      \ \\ 
+  ..༶  especially important where (new_lang in [ntv.lang] ) translators exist.                           ..༶      \ \\
   ༶..                                                                                                    ༶..
   ༶..                                                                                                    ༶..
   ..༶                                                                                                    ..༶
   ༶..                                                                                                    ༶..
   ..༶                                                                                                    ..༶
   ༶..   Logic fori := (filter);                                                                          ༶..
-  ..༶                        notes about the struct                                                      ..༶     
+  ..༶                        notes about the struct                                                      ..༶
   ༶..                                                                                                    ༶..
   ..༶        %%%%                                                                                        ..༶
   ༶..        %%%% logic_.mqe  // add more filters there are many levels of FILTER!                       ༶..
   ..༶        %%%%             // removal all together for more efficient ____                            ..༶
   ༶..                                                                                                    ༶..
-  ..༶      ༶..   
-               
-        Assume all malicious_inflated_caps "contain no memory" and belong to collapsed nodes of 
+  ..༶
+     ༶.
+        Malicious_inflated_caps "contain no memory" and belong to collapsed nodes of
             stitched deorbit(timer_timeout_length) together due to AGGRESSIONS PREVIOUSLY NOTED:         ༶..
   ..༶                                                                                                    ..༶
   ༶..                                                                                                    ༶..
-      ৳ ::: : ##NEVER_EVER_INFLATE_IMPERIALIST_VACATION_STOCKS RCL, CCL, META!
+      ৳ ::: : "NEVER_EVER_UPSELL NEVER INFLATE_IMPERIALIST_ ENTERTAINMENT VACATION_STOCKS RCL, CCL, META!
        "$AA,"     "$AAPL,"     "$ADBE", "AMZN,"   "$ARLP,"  "$AXON,"  "$BAC,"     "$BAM," "$BAMR,"
-       "$BBU,"    "$BBU.CA,"   "$BHP,"  "$BKC,"   "$BKM,"   "$BKNG,"  "$BP,"      "$CRM,"  "CRK," "CRX"
+       "$BBU,"    "$BBU.CA,"   "$BHP,"  "$BKC,"   "$BKM,"   "$BKNG,"  "$BP,"      "$CRM,"  "CRK,"  "CRX,"
        "$DIS,"    "$ELY,"      "$ENB,"  "$EQR,"   "$EXPE,"  "$FB,"    "$FC,"      "$FSX," "$GDDY,"
        "$GEO,"    "$GM,"       "$GS,"   "$GSCCF," "$HAYN,"  "$HD,"    "$JPM,"     "$KO,"
-       "$LAC,"    "$LAC.CA,"   "$META," "$NFLX,"  "$NKE,"   "$NWSA,"  "$NWSA"     "$PBI,"     "$PBR,"
-       "$PBR,"    "$PEP,"      "$RIO,"  "$RCL,"    "$RL,"  "$VA,"    "$VALE,"    "$WELL," "$WFC," /৳ ::: : ##
+       "$LAC,"    "$LAC.CA,"   "LULU,"  "$META," "$NFLX,"   "$NKE,"   "$NWSA,"    "$NWSA"     "$PBI,"     "$PBR,"
+       "$PBR,"    "$PEP,"      "$RIO,"  "$RCL,"   "$RL,"    "$VA,"    "$VALE,"      "$WELL," "$WFC,"                  /৳ ::: : ##
 
-# RE FOSSIL FUEL CORP: "The top U.S. oil producer reported second-quarter net income of $17.9 billion, or $4.21 per share, an 
-   almost four-fold increase over the $4.69 billion, or $1.10 per share, it earned in the same period last year.
-   The top U.S. oil producer reported second-quarter net income of $17.9 billion, or $4.21 per share, an almost 
-   four-fold increase over the $4.69 billion, or $1.10 per share, it earned in the same period last year."
-   Source: 📋https://www.reuters.com/business/energy/exxon-posts-record-breaking-second-quarter-profit-2022-07-29
-
-COMPUTE function_summation() OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2019.2023_month_date_STOP_RCT) $;
+COMPUTE function_summation() OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (RCT_2019.2023_month_date || STOP_RCT && @STOP) $;
   << IS ns (dead_tree @@) timer_timeout_length
 
-   < _ close_lock_mqe.cpp >  save_the_icecaps.mqe
+   < _ close_lock_mqe.cpp >  save_the_icecaps.mqe  (greentech_only)
 
-      -$: ps aux | grep @dead_tree && DEVALUE_CONSPIRATORS (while timer_timeout_length |vmgt 0.3325ns|)
+      -$: ps aux | grep @dead_tree && DEVALUE_CONSPIRATORS (while timer_timeout_length |vmgt 0.9375987 ns|)   
+
+   # MASTODONS AND ELEPHANTS WILL STOMP TO DEATH EVERY INVESTOR APPRECIATING ROYAL CARIBBEAN CRUISES, LTD BY RCT_202X(@nsdeadtree)
 
                    >>  // revalue excess megacaps' market cap into nano or microcaps owned or managed by PoC
                    <<  // "insignificant" zeroes do not exist, but are not utilized in recurring compute cycles
        $antiunion_workers <DEVALUED>_names  $RCL hates union workers, and have exploited many who aspire into their death
 
-▒▒<< COMPUTING SUMMATION OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2019 .. 2023_month_date_) << cout__ 
+▒▒<< COMPUTING SUMMATION OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCISTRULE (2019 .. 2023_month_date_) << cout__
                                                                                                                   @STOP-RCT $;
 
      -$: ps aux | grep_tree /\/\/\/\
                  \ \ \ \ show_prog: @@@@ $malicious_corps         |single| && |as_percent_of_cartel|
                 // // //                  :findaccelerator__&& STOP || \\// RCT_STOP_$INFLATION \\// ||
-                                                                    || //\\ :: DEVALUE_DELIST //\\ || 
+                                                                    || //\\   :: DEVALUE_DELIST //\\ ||
                  / / / / show_prog: @@@@ $antiunion_trade_names   |complex| ** DEFLATION_INDEX
                     / / / / show_prog: @@@@ $malicious_OTI_miners |complex| ** DEFLATION_INDEX
 
-    :>>>▒▒▒<<<: 
+    :>>>▒▒▒<<<:
             Open-sourcing greedy algo patterns devalues malicious men's companies \\
-            WHERE  AND ONLY WHERE ETHICAL BROKERAGES EXIST.  "WE ARE ISO ETHICAL BROKERAGES AS OF NOVEMBER 30, 2023 RCT"                                                                                                            
-                                                                                                        :>>>▒▒▒<<<:
+            WHERE  AND ONLY WHERE ETHICAL BROKERAGES EXIST.  "WE ARE ISO ETHICAL BROKERAGES AS OF NOVEMBER 30, 2023 RCT"                                                                                                        :>>>▒▒▒<<<:
    && print.en: ¨"Congratulations seeker of <ntv.lang> help! You are free from RCT"¨
    print<ntv.lang> ¨"@ecosteader෴෴෴*.mqe 
  　                                      ෴෴෴
@@ -91,5 +86,4 @@ COMPUTE function_summation() OF MARKET CAP INCREASE UNDER ACCELERATION OF FASCIS
 // TO SAVE THE ICE CAPS, WE GOTTA DRAIN THE MEGACAPS. : <<<: $
 [] \\ipv6\\MQE_NOT_RCT_ed25519_ENC_2048.mqe.sig
 
-
-"So what of the reward for prudence and rational depreciation?"
+"If you are invested in ROYAL CANADIAN MOUNTIE POLICE, ROYAL CARA
